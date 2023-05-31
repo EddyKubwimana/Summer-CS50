d81cd9dc680a2d305d7393ced3e002f61777e03d
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-05-31 01:24:20 +0000

    NEW CHANGES

---
+++ JS/app.js
@@ -0,0 +1,28 @@
+$(document).ready (function(){
+    //owl 
+    $('.owl-carousel').owlCarousel({
+        margin :5,
+        navigation:true,
+        loop:true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        
+        responsive:{
+            0:{
+                items:1,
+                nav:false,
+
+            },
+            600:{
+                items:1,
+                nav:false,
+             },
+
+             1000:{
+                items:3,
+                
+             }
+
+
+    })
+})
